2dc86d13f8a5010e1987d3d07211dd58c6bdbc89
namgigun <84756980+namgigun@users.noreply.github.com>
2025-04-19 17:53:28 +0900

    feat : 로그인 실패시 에러 메시지 추가

--- src/main/java/Toyprojects/CatchABeer/config/SecurityConfig.java
+++ src/main/java/Toyprojects/CatchABeer/config/SecurityConfig.java
@@ -1,14 +1,19 @@
 package Toyprojects.CatchABeer.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 
 @Configuration
+@RequiredArgsConstructor
 public class SecurityConfig {
+    private final AuthenticationFailureHandler customFailureHandler;
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
         http
@@ -20,6 +25,7 @@ public class SecurityConfig {
                         .loginPage("/")
                         .loginProcessingUrl("/login")
                         .defaultSuccessUrl("/moveRoom")
+                        .failureHandler(customFailureHandler)
                 );
 
         return http.build();
--- src/main/java/Toyprojects/CatchABeer/controller/MemberController.java
+++ src/main/java/Toyprojects/CatchABeer/controller/MemberController.java
@@ -7,9 +7,11 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 //@RestController
 @Controller
@@ -35,7 +37,11 @@ public class MemberController {
 
     // 로그인 화면 호출
     @GetMapping("/")
-    public String login() {
+    public String login(@RequestParam(value = "error", required = false)String error,
+                             @RequestParam(value = "exception", required = false)String exception,
+                             Model model) {
+        model.addAttribute("error", error);
+        model.addAttribute("exception", exception);
         return "loginForm";
     }
 }
